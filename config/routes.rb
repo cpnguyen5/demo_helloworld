1106d919822143aa5ceac7635dc6346185fb7941
cpnguyen <cpnguyen5@dons.usfca.edu>
2017-06-10 14:21:54 -0700

    Change rendering of root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Root is base URL ("home page")
+  root 'application#hello' # 'controller_name#action_name'
 end
